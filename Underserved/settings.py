f0915bf2643534c284a9be03faae5c78e7e53f7c
Jake Chavez <jake94chavez@gmail.com>
2018-02-17 10:29:05 -0600

    set up db for app

--- Underserved/settings.py
+++ Underserved/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'Underserved.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'underserved_users',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
