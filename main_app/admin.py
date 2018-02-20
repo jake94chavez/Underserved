5bd923ae64411efb7c1dad4336ab193fa8f9dbca
Jake Chavez <jake94chavez@gmail.com>
2018-02-20 13:32:20 -0600

    add user model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import Filters
+from.models import Games
+from.models import Index
 
 # Register your models here.
+admin.site.register(Filters)
+admin.site.register(Games)
+admin.site.register(Index)
--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Filters(models.Model):
@@ -7,15 +8,20 @@ class Filters(models.Model):
 	label = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
+
 class Games(models.Model):
 	app_id = models.IntegerField(max_digits=10)
 	app_name = models.CharField(max_length=100)
 	metascore = models.IntegerField(max_digits=3)
 	def __str__(self):
 		return self.app_name
+
 class Index(models.Model):
 	kind = models.CharField(max_length=1)
 	name = models.CharField(max_length=100)
 	app_id = models.IntegerField(max_digits=10)
 	def __str__(self):
 		return self.name
+		
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
