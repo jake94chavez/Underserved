11d6802041bf4fe319849a47f52fe31d166dba9c
Jake Chavez <jake94chavez@gmail.com>
2018-02-20 14:35:25 -0600

    all models properly working and migrated, seed WIP

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Filters(models.Model):
+class Filter(models.Model):
 	kind = models.CharField(max_length=1)
 	name = models.CharField(max_length=100)
 	label = models.CharField(max_length=100)
 	def __str__(self):
 		return self.name
 
-class Games(models.Model):
+class Game(models.Model):
 	app_id = models.IntegerField()
 	app_name = models.CharField(max_length=100)
 	metascore = models.IntegerField()
