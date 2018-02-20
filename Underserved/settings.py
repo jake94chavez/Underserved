2911c401e286c95f736ed530f442224048db74f4
Marcy Acevedo <marcylinaacevedo@gmail.com>
2018-02-20 11:36:01 -0600

    starts url paths. index page renders

--- Underserved/settings.py
+++ Underserved/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Underserved/urls.py
+++ Underserved/urls.py
@@ -1,8 +1,9 @@
 
-
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from main_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	url(r'admin/', admin.site.urls),
+	url(r'', views.index)
 ]
