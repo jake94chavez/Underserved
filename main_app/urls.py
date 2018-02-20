ab3e0fafaa8006738725bdf8a8dc753a5915189c
Marcy Acevedo <marcylinaacevedo@gmail.com>
2018-02-20 13:47:38 -0600

    results page renders. starts reformatting pages to look better

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from . import views
 from .views import about
+from .views import results
 
 # from views import about
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^about/$', about),
+    url(r'^results/$', results),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def about(request):
 	return render(request, 'about.html')
+
+def results(request):
+	return render(request, 'results.html')
